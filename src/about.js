c3dcb9bd3277f4f540cbfb348f55ed75d59235ea
danielld75 <danielld@poczta.fm>
2019-12-12 16:37:43 +0100

    add about

---
+++ src/about.js
@@ -0,0 +1,26 @@
+import React from "react";
+
+const about = () => {
+    return (
+        <div>
+            <h1>CANTUS MIRABILIS</h1>
+            <h3>Jesteśmy zespołem wokalnym działającym przy katedrze Legnickiej</h3>
+            <a
+                className="App-link"
+                href="https://www.youtube.com/channel/UCew5gF2HiBoV8-okJI9H6dw/featured"
+                target="_blank"
+                rel="noopener noreferrer"
+            > Posłuchaj nas na YT
+            </a>
+            <a
+                className="App-link"
+                href="https://www.facebook.com/cantusMirabilisLegnica/"
+                target="_blank"
+                rel="noopener noreferrer"
+            > Facebook
+            </a>
+        </div>
+    )
+};
+
+export default about;
