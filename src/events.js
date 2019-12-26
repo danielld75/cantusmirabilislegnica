64940a09fa61b1fa0a8c787a9e9c642b3fd8225f
danielld75 <danielld@poczta.fm>
2019-12-26 10:08:03 +0100

    events add 22.12

--- src/events.js
+++ src/events.js
@@ -5,8 +5,11 @@ const events = () => {
         <div className={'App-events'}>
             <h3>Wydarzenia:</h3>
             <ul>
-                <li>14 XII 2019 godz. 18<sup>00</sup> - Msza święta (Peregrynacja Matki Bożej Kodeńskiej) w parafii św. Józefa w Legnicy</li>
-                <li>22 XII 2019 godz. 17<sup>00</sup> - Adoracja Najświętszego Sakramentu, Katedra Legnicka</li>
+                <li>29 XII 2019 godz. 10<sup>00</sup> - msza święta w Niedzielę św. Rodziny, Katedra Legnicka</li>
+                <div style={{color:'#777'}}>
+                    <li>22 XII 2019 godz. 17<sup>00</sup> - Adoracja Najświętszego Sakramentu, Katedra Legnicka</li>
+                    <li>14 XII 2019 godz. 18<sup>00</sup> - Msza święta (Peregrynacja Matki Bożej Kodeńskiej) w parafii św. Józefa w Legnicy</li>
+                </div>
             </ul>
         </div>
     )
