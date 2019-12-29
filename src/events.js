2751adedcaa7c7d51b3f41f990cd9df285ee5958
danielld75 <danielld@poczta.fm>
2019-12-29 13:17:12 +0100

    koncert koled

--- src/events.js
+++ src/events.js
@@ -5,8 +5,9 @@ const events = () => {
         <div className={'App-events'}>
             <h3>Wydarzenia:</h3>
             <ul>
-                <li>29 XII 2019 godz. 10<sup>00</sup> - msza święta w Niedzielę św. Rodziny, Katedra Legnicka</li>
+                <li>Koncert Kolęd 2020</li>
                 <div style={{color:'#777'}}>
+                    <li>29 XII 2019 godz. 10<sup>00</sup> - msza święta w Niedzielę św. Rodziny, Katedra Legnicka</li>
                     <li>22 XII 2019 godz. 17<sup>00</sup> - Adoracja Najświętszego Sakramentu, Katedra Legnicka</li>
                     <li>14 XII 2019 godz. 18<sup>00</sup> - Msza święta (Peregrynacja Matki Bożej Kodeńskiej) w parafii św. Józefa w Legnicy</li>
                 </div>
